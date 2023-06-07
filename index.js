6bab72de084a8f886432fb78f8612757252c6a81
Amrit Anand Singh <amritanandsingh@Amrits-MacBook-Air.local>
2023-06-07 15:09:55 +0530

    file system is done for Quora

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 const express = require('express');
 const app = express();
-
+const expressLayouts = require('express-ejs-layouts');    // for layouts
 const port = 3000;
+app.use(express.static('./assets'));       // set style for layout , header , footer 
+app.use(expressLayouts);                 // to merge headerfooter and display in layout.ejs
+app.set('layout extractStyles',true);   // to set style link in top of layout.ejs 
+app.set('layout extractScripts',true); // to set script link in top of layout.ejs
+app.use('/',require('./routes'));                   
+app.set('view engine','ejs'); 
+app.set('views','./views');     
+
 
 app.listen(port , function(err){
+    
     if(err)
     {
         console.log(`server is facing error: ${err}`);
     }
-    console.log("server is up and running ");
+    console.log(`server is up and running ${port}` );
 });  
  
---
+++ routes/index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const homeController = require('../controllers/home_controller');
+const router = express.Router();
+console.log("router is loaded");
+
+router.get('/',homeController.home);
+router.use('/users',require('./users'));
+module.exports=router;
