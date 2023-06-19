f0cc48aa22cff030b40c650504b7be23020129d5
Amrit Anand Singh <amritanandsingh@Amrits-MacBook-Air.local>
2023-06-20 00:02:59 +0530

    print the name of user who ask the question also fix issue in post user id in post table was not saving

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,3 +1,45 @@
-module.exports.home=function(req,res){
-    return res.render('home',{title:'Quora'});
-}
+const Post = require('../models/post');
+const User = require('../models/user');
+
+/*
+module.exports.home = async function(req, res) {
+    try {
+        const posts = await Post.find({}).exec();
+        return res.render('home', {
+            title: 'Quora | Home',
+            posts: posts ,
+            .sort('-createdAt')
+            .populate('user')
+                path:'User',
+                populate: {
+                    path:'name'}
+        });
+    } catch (err) {
+        console.log('Error in finding posts:', err);
+        return res.redirect('back');
+    }
+};
+*/
+module.exports.home = async function(req, res) {
+    try {
+      const posts = await Post.find({})
+        .sort('-createdAt')
+        .populate("user")
+        .populate({
+          path: 'user',
+          populate: {
+            path: 'name'
+          }
+        })
+        .exec();
+       
+      return res.render('home', {
+        title: 'Quora | Home',
+        posts: posts,
+        
+      });
+    } catch (err) {
+      console.log('Error in finding posts:', err);
+      return res.redirect('back');
+    }
+  };
--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -2,10 +2,13 @@ const { renderFile } = require('ejs');
 const Post= require('../models/post');
 
 module.exports.create = function(req, res) {
+    console.log(req.body);
+    console.log(req.user._id);
+    
     Post.create({
         title: req.body.title,
         content: req.body.content,
-        user: req.body._id
+        user: req.user._id
     })
     .then(function(post) {
         // Post creation successful
